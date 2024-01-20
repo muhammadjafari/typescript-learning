89fa4cd94058b5c5323ba737c9f4d7d54c73d760
Mohammad Jafari <jafariimuhammad@gmail.com>
2024-01-20 12:49:24 +0330

    added types to function

---
+++ app.js
@@ -0,0 +1,6 @@
+function add(n1, n2) {
+    return n1 + n2;
+}
+var num1 = 3;
+var num2 = 4;
+console.log(add(num1, num2));
---
+++ app.ts
@@ -0,0 +1,8 @@
+function add(n1: number, n2: number) {
+  return n1 + n2;
+}
+
+const num1 = 3;
+const num2 = 4;
+
+console.log(add(num1, num2));
