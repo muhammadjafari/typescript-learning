0c675887418faa0f271b9d1f8b20965fb232fcb1
Mohammad Jafari <jafariimuhammad@gmail.com>
2024-02-05 11:52:36 +0330

    array types

--- app.js
+++ app.js
@@ -7,7 +7,21 @@
 //   name: "Max",
 //   age: 26,
 // };
-var person = {
-    name: "Max",
-    age: 26,
-};
+// const person: { name: string; age: number } = {
+//   name: "Max",
+//   age: 26,
+// };
+// const person = {
+//   name: "Max",
+//   age: 26,
+//   hobbies: ["sports", "movies"],
+// };
+// for (const hobby of person.hobbies) {
+//   console.log(hobby);
+// }
+var favoriteSports;
+favoriteSports = ["basketball", "football"];
+for (var _i = 0, favoriteSports_1 = favoriteSports; _i < favoriteSports_1.length; _i++) {
+    var sport = favoriteSports_1[_i];
+    console.log(sport.toUpperCase());
+}
--- app.ts
+++ app.ts
@@ -10,7 +10,25 @@
 //   age: 26,
 // };
 
-const person: { name: string; age: number } = {
-  name: "Max",
-  age: 26,
-};
+// const person: { name: string; age: number } = {
+//   name: "Max",
+//   age: 26,
+// };
+
+// const person = {
+//   name: "Max",
+//   age: 26,
+//   hobbies: ["sports", "movies"],
+// };
+
+// for (const hobby of person.hobbies) {
+//   console.log(hobby);
+// }
+
+let favoriteSports: string[];
+
+favoriteSports = ["basketball", "football"];
+
+for (const sport of favoriteSports) {
+  console.log(sport.toUpperCase());
+}
