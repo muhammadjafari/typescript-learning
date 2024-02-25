afbca94f534032d5234815f51ac606c0303ad000
Mohammad Jafari <jafariimuhammad@gmail.com>
2024-02-25 12:23:13 +0330

    type guard

--- dist/app.js
+++ dist/app.js
@@ -4,3 +4,33 @@ const e1 = {
     privileges: ["create-server"],
     startDate: new Date(),
 };
+function add(a, b) {
+    if (typeof a === "string" || typeof b === "string") {
+        return a.toString() + b.toString();
+    }
+    return a + b;
+}
+function printEmployeeInfo(emp) {
+    if ("startDate" in emp) {
+        console.log(emp.startDate);
+    }
+}
+class Car {
+    drive() {
+        console.log("driving ...");
+    }
+}
+class Truck {
+    drive() {
+        console.log("driving ...");
+    }
+    loadCargo() {
+        console.log("loading ...");
+    }
+}
+function useVehicle(vehicle) {
+    vehicle.drive();
+    if (vehicle instanceof Truck) {
+        vehicle.loadCargo();
+    }
+}
--- src/app.ts
+++ src/app.ts
@@ -20,3 +20,43 @@ type Combinable = string | number;
 type Numeric = number | boolean;
 
 type Universal = Combinable & Numeric;
+
+function add(a: Combinable, b: Combinable) {
+  if (typeof a === "string" || typeof b === "string") {
+    return a.toString() + b.toString();
+  }
+  return a + b;
+}
+
+type unknownEmployee = Admin | Employee;
+
+function printEmployeeInfo(emp: unknownEmployee) {
+  if ("startDate" in emp) {
+    console.log(emp.startDate);
+  }
+}
+
+class Car {
+  drive() {
+    console.log("driving ...");
+  }
+}
+
+class Truck {
+  drive() {
+    console.log("driving ...");
+  }
+
+  loadCargo() {
+    console.log("loading ...");
+  }
+}
+
+type Vehicle = Car | Truck;
+
+function useVehicle(vehicle: Vehicle) {
+  vehicle.drive();
+  if (vehicle instanceof Truck) {
+    vehicle.loadCargo();
+  }
+}
