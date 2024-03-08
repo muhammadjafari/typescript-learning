af8c3b2263dbed8e2c41fcc62f56c94595557dd7
Mohammad Jafari <jafariimuhammad@gmail.com>
2024-03-08 12:41:03 +0330

    generic classes

--- dist/app.js
+++ dist/app.js
@@ -9,7 +9,7 @@
 function merge(obj1, obj2) {
     return Object.assign(obj1, obj2);
 }
-const objMerged = merge({ name: 'Max' }, { age: 30 });
+const objMerged = merge({ name: "Max" }, { age: 30 });
 console.log(objMerged.name);
 function countAndDescribe(element) {
     console.log(element.length);
@@ -18,3 +18,22 @@ countAndDescribe("hello world!");
 function extractAndConvert(obj, key) {
     console.log(obj[key]);
 }
+class storageData {
+    constructor() {
+        this.data = [];
+    }
+    addItem(item) {
+        this.data.push(item);
+    }
+    removeItem(item) {
+        this.data.splice(this.data.indexOf(item), 1);
+    }
+    getItems() {
+        console.log([...this.data]);
+    }
+}
+const textStorage = new storageData();
+textStorage.addItem("Max");
+textStorage.addItem("Manu");
+textStorage.removeItem("Max");
+textStorage.getItems();
--- src/app.ts
+++ src/app.ts
@@ -8,24 +8,48 @@
 //   data.split('')
 // })
 
-function merge<T extends {},U extends {}>(obj1: T,obj2: U){
-  return Object.assign(obj1,obj2)
+function merge<T extends {}, U extends {}>(obj1: T, obj2: U) {
+  return Object.assign(obj1, obj2);
 }
 
-const objMerged = merge({name:'Max'},{age:30})
+const objMerged = merge({ name: "Max" }, { age: 30 });
 console.log(objMerged.name);
 
 interface lengthy {
   length: number;
 }
 
-function countAndDescribe<T extends lengthy>(element: T): void{
+function countAndDescribe<T extends lengthy>(element: T): void {
   console.log(element.length);
-  
 }
 
 countAndDescribe("hello world!");
 
-function extractAndConvert<T extends object, U extends keyof T>(obj: T,key: U){
+function extractAndConvert<T extends object, U extends keyof T>(
+  obj: T,
+  key: U
+) {
   console.log(obj[key]);
 }
+
+class storageData<T extends string | number> {
+  private data: T[] = [];
+
+  addItem(item: T) {
+    this.data.push(item);
+  }
+
+  removeItem(item: T) {
+    this.data.splice(this.data.indexOf(item), 1);
+  }
+
+  getItems() {
+    console.log([...this.data]);
+  }
+}
+
+const textStorage = new storageData();
+textStorage.addItem("Max");
+textStorage.addItem("Manu");
+textStorage.removeItem("Max");
+textStorage.getItems();
